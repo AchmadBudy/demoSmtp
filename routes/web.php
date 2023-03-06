485a9b2f3ba2fdba29b4b34c18df0eb9a210068c
achmadbudy <nurachmadbudykurniawan@gmail.com>
2023-03-06 16:58:58 +0700

    setup smtp

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\OrderShipped;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/kirim', function () {
+    Mail::to('achmadwawan2@gmail.com')->send(new OrderShipped());
 });
